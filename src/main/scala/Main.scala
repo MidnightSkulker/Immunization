2301db4d69dd49ace6616b5adb4d7531e286a4c3
Peter White <desourdesourde@gmail.com>
2020-12-28 00:43:30 -0800

    Moves Doses classes to a separate package

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,12 +11,7 @@ import scala.util.matching.Regex
 import models.model._
 import json._
 import rules._
-
-trait AgeRange {
-  def withinRange(d: DateTime, dob: DateTime, startMonth: Int, endMonth: Int): Boolean = {
-    (d.isAfter(dob.plusMonths(startMonth)) && (d.isBefore(dob.plusMonths(endMonth))))
-  }
-}
+import doses._
 
 trait Older {
   def olderThan(dob: DateTime, numberOfMonths: Int): Boolean =
@@ -36,34 +31,6 @@ trait Recently {
 trait NewBorn {
   // Determine if the child is newly born.
   def isNewBorn (dob: DateTime): Boolean = dob.plusMonths(2).isAfter(DateTime.now())
-}
-
-class DoseMap(name: String, doses: DateMap) {
-  // Get the nth dose, using ordinal numbers. All the elements of the map are
-  // assumed to have the a name in a series, such as "dtap1", "dtap2", ...
-  def nth(n: Int) = doses(name + n)
-  def firstDose: DateTime = nth(1)
-  def secondDose: DateTime = nth(2)
-  def thirdDose: DateTime = nth(3)
-  def fourthDose:DateTime =  nth(4)
-  def fifthDose: DateTime = nth(5)
-}
-
-// Common operations on the doses given.
-class Doses(name: String, dob: DateTime, doses: DateMap)
-    extends DoseMap(name, doses) with AgeRange {
-  def doseNwithinAgePeriod(dose: Int, startMonth: Int, endMonth: Int): Boolean =
-    withinRange(nth(dose), dob, startMonth, endMonth)
-  def doseIsAfter(dose: DateTime, dob: DateTime, nMonths: Int): Boolean =
-    dose.isAfter(dob.plusMonths(nMonths))
-  def doseIsBefore(dose: DateTime, dob: DateTime, nMonths: Int): Boolean =
-    dose.isBefore(dob.plusMonths(nMonths))
-  // Determine if one dose is after another plus a number of days.
-  def doseAfterDose(dose1: DateTime, dose2: DateTime, days: Int): Boolean =
-    dose2.isAfter(dose1.plusDays(days))
-  // Determine if one dose is before another plus a number of days.
-  def doseBeforeDose(dose1: DateTime, dose2: DateTime, days: Int): Boolean =
-    dose2.isBefore(dose1.plusDays(days))
 }
 
 // Vaccinations
